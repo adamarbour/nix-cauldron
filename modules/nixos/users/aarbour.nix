13f08430d37395ab0e465d599fe5b91fee6a40b5
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2025-09-17 03:28:31 -0500

    Pull keys from github

--- modules/nixos/users/aarbour.nix
+++ modules/nixos/users/aarbour.nix
@@ -6,6 +6,12 @@ in {
     users.users.aarbour = {
       # Initial throwaway password: "nixos"
       initialHashedPassword = mkDefault "$y$j9T$FbXu9/hYPFtVkAy.3JSCs1$XAgWbQs7MbNHP/jH3LRYoxzcwhpQAjY74U7fv40XO94";
+      openssh.authorizedKeys.keyFiles = [
+        (builtins.fetchurl {
+          url = "https://github.com/adamarbour.keys";
+          sha256 = "sha256-yT6FMVq81ASQo2ILGOl3pwYuyDf8SM+QUdhoDH6bdOA=";
+        })
+      ];
     };
   };
 }
