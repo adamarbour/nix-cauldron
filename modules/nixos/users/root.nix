a1eccd3334a8fd4da5265908ef5087b7f34cb05e
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2025-09-24 23:57:10 -0500

    Ensured noshell doesn't override bash for root

--- modules/nixos/users/root.nix
+++ modules/nixos/users/root.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ lib, pkg, config, ... }:
 let
   inherit (lib) mkIf mkDefault;
 in {  
@@ -8,6 +8,7 @@ in {
       initialHashedPassword = mkDefault "$y$j9T$FbXu9/hYPFtVkAy.3JSCs1$XAgWbQs7MbNHP/jH3LRYoxzcwhpQAjY74U7fv40XO94";
       hashedPasswordFile = mkIf config.cauldron.secrets.enable
         config.users.users.${config.cauldron.host.mainUser}.hashedPasswordFile;
+    		shell = pkgs.bashInteractive;
     };
   };
 }
