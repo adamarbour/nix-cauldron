5ac282ddc6a584f230ee9a642350ec5f5739fc98
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2025-09-26 17:12:13 -0500

    fix(users): conditional secrets

--- modules/nixos/users/aarbour.nix
+++ modules/nixos/users/aarbour.nix
@@ -1,17 +1,22 @@
 { lib, pkgs, config, ... }:
 let
-  inherit (lib) elem mkIf mkDefault;
+  inherit (lib) elem mkIf mkDefault mkMerge;
+  secrets = config.cauldron.secrets;
 in {
   config = mkIf (elem "aarbour" config.cauldron.host.users) {
-    users.users.aarbour = {
-      # Initial throwaway password: "nixos"
-      initialHashedPassword = mkDefault "$y$j9T$FbXu9/hYPFtVkAy.3JSCs1$XAgWbQs7MbNHP/jH3LRYoxzcwhpQAjY74U7fv40XO94";
-      openssh.authorizedKeys.keyFiles = [
-        (builtins.fetchurl {
-          url = "https://github.com/adamarbour.keys";
-          sha256 = "sha256-yT6FMVq81ASQo2ILGOl3pwYuyDf8SM+QUdhoDH6bdOA=";
-        })
-      ];
-    };
+    users.users.aarbour = mkMerge [
+    		{
+    			openssh.authorizedKeys.keyFiles = [
+		      (builtins.fetchurl {
+		        url = "https://github.com/adamarbour.keys";
+		        sha256 = "sha256-yT6FMVq81ASQo2ILGOl3pwYuyDf8SM+QUdhoDH6bdOA=";
+		      })
+		    ];
+    		}
+    		(mkIf (!secrets.enable) {
+    			# Initial throwaway password: "nixos"
+      		initialHashedPassword = mkDefault "$y$j9T$FbXu9/hYPFtVkAy.3JSCs1$XAgWbQs7MbNHP/jH3LRYoxzcwhpQAjY74U7fv40XO94";
+    		})
+    ];
   };
 }
--- modules/nixos/users/root.nix
+++ modules/nixos/users/root.nix
@@ -1,14 +1,18 @@
 { lib, pkgs, config, ... }:
 let
-  inherit (lib) mkIf mkDefault;
+  inherit (lib) mkIf mkDefault mkMerge;
+  secrets = config.cauldron.secrets;
 in {  
   config = {
-    users.users.root = {
-      # Initial throwaway password: "nixos"
-      initialHashedPassword = mkDefault "$y$j9T$FbXu9/hYPFtVkAy.3JSCs1$XAgWbQs7MbNHP/jH3LRYoxzcwhpQAjY74U7fv40XO94";
-      hashedPasswordFile = mkIf config.cauldron.secrets.enable
-        config.users.users.${config.cauldron.host.mainUser}.hashedPasswordFile;
-    		shell = pkgs.bashInteractive;
-    };
+    users.users.root = mkMerge [
+    		{ shell = pkgs.bashInteractive; }
+    		(mkIf (secrets.enable) {
+    			hashedPasswordFile = config.users.users.${config.cauldron.host.mainUser}.hashedPasswordFile;
+    		})
+    		(mkIf (!secrets.enable) {
+    			# Initial throwaway password: "nixos"
+      		initialHashedPassword = mkDefault "$y$j9T$FbXu9/hYPFtVkAy.3JSCs1$XAgWbQs7MbNHP/jH3LRYoxzcwhpQAjY74U7fv40XO94";
+    		})
+    ];
   };
 }
