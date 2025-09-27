03c45c201f725cb99df5208d14b923be92a7677e
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2025-09-26 21:14:19 -0500

    fix(wg): Removing morrigan from network

--- systems/morrigan/configuration.nix
+++ systems/morrigan/configuration.nix
@@ -28,25 +28,6 @@
       };
       network = {
         wireless.backend = "iwd";
-         wireguard.tunnels = {
-          "arbour-cloud" = {
-            publicKey = "qBd0mGQuBCxL3Pl401UHTt6Ng1PjbCPRz56vppiwfG8=";
-            privateKey = { kind = "sops"; path = "wg/morrigan.key"; };
-            addresses = [ "172.31.7.101/32" "2001:db8:ac::101/128"];
-            routes = [
-              { Destination = "172.31.7.0/24"; }
-              { Destination = "2001:db8:ac::/64"; }
-            ];
-          };
-          "nflix" = {
-            publicKey = "qBd0mGQuBCxL3Pl401UHTt6Ng1PjbCPRz56vppiwfG8=";
-            privateKey = { kind = "sops"; path = "wg/morrigan.key"; };
-            addresses = [ "10.11.12.1/32" ];
-            routes = [
-              { Destination = "10.11.12.0/24"; }
-            ];
-          };
-        };
       };
       feature = {
         touchscreen = {
