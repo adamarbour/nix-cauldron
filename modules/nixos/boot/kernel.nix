c1c6bf35ac371bd0339513ab5d4c02c86a9b6b2b
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2025-09-28 16:25:34 -0500

    fix(kernel): Settled...

--- modules/nixos/boot/kernel.nix
+++ modules/nixos/boot/kernel.nix
@@ -3,24 +3,19 @@ let
   inherit (lib) types mkOption mkOverride mapAttrsToList;
   inherit (lib.cauldron) hasProfile;
   cfg = config.cauldron.host.boot;
-  chaotic = (import sources.flake-compat { src = sources.chaotic-nyx; }).outputs;
   
   defaultKernel = if (hasProfile config "gaming") then pkgs.linuxPackages_xanmod_latest
     else if (hasProfile config "workstation") then pkgs.linuxPackages_latest_hardened
     else if (hasProfile config "server") then pkgs.linuxPackages_hardened
-    else if (hasProfile config "graphical") then pkgs.linuxPackages_xanmod_stable
+    else if (hasProfile config "graphical") then pkgs.linuxPackages_xanmod
     else pkgs.linuxPackages_latest;
+    
 in {
-  imports = [
-    chaotic.nixosModules.nyx-cache
-    chaotic.nixosModules.nyx-overlay
-    chaotic.nixosModules.nyx-registry
-  ];
   
   options.cauldron.host.boot = {
     kernel = mkOption {
       type = types.raw;
-      default = pkgs.linuxPackages_latest;
+      default = defaultKernel;
       defaultText = "${defaultKernel}";
       description = "The kernel to use for the system.";
     };
@@ -29,15 +24,6 @@ in {
   config = {
     boot = {
       kernelPackages = mkOverride 500 cfg.kernel;
-      # We like the bore scheduler...
-      kernelPatches = let
-        patchesDir = "${sources.bore}/patches/stable/linux-${lib.versions.majorMinor config.boot.kernelPackages.kernel.version}-bore";
-      in lib.optionals (hasProfile config "gaming") (
-        mapAttrsToList (name: _: {
-          name = "bore-${name}";
-          patch = "${patchesDir}/${name}";
-        }) (builtins.readDir patchesDir)
-      );
     };
   };
 }
