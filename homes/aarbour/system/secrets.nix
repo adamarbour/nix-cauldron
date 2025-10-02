32407086c1b57976818b76b6c65d8b3b74d7ca01
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2025-10-01 21:03:18 -0500

    fix(home:aarbour): conditional secrets

--- homes/aarbour/system/secrets.nix
+++ homes/aarbour/system/secrets.nix
@@ -1,6 +1,9 @@
-{ config, ... }:
-{
-  sops.secrets = {
+{ lib, config, osConfig, ... }:
+let
+  inherit (lib) mkIf;
+  cfg = osConfig.cauldron.secrets;
+in {
+  sops.secrets = mkIf cfg.enable {
     age_key = {
       path = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
     };
