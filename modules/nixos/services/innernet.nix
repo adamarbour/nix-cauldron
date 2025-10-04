89e53eb645c40d90b4bd70086e9fff875ac526ac
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2025-10-04 00:22:40 -0500

    fix(innernet): Server unit. Will fix client unit later...

--- modules/nixos/services/innernet.nix
+++ modules/nixos/services/innernet.nix
@@ -1,7 +1,6 @@
 { lib, pkgs, config, ... }:
 let
-  inherit (lib) types mkIf mkEnableOption;
-  
+  inherit (lib) types mkIf mkEnableOption mkOption;
   netName = cfg.name;
   
   cfg = config.cauldron.services.innernet;
@@ -18,19 +17,29 @@ in {
       default = "innernet0";
       description = "Overlay network name (becomes innernet@<name>).";
     };
-    configDir = mkOption {
+    privateKeyFile = mkOption {
       type = path;
-      default = "/etc/innernet";
-      description = "Directory for client configs (invitation TOML installs land here).";
+      description = "The path to the private key file";
+      example = "/run/secrets/server-private-key";
     };
-    stateDir = mkOption {
-      type = path;
-      default = "/var/lib/innernet";
-      description = "Server state directory (SQLite DB, keys, etc.).";
+    publicKey = mkOption {
+      type = str;
+      description = "The public key of the server";
+      example = "wLKnL8rBNdSV7HBeuJLS6inBsqbqEwCexN+6jAbMfxk=";
     };
     # SERVER
     server = {
       enable = mkEnableOption "Run innernet registry (innernet-server).";
+      configDir = mkOption {
+        type = path;
+        default = "/etc/innernet-server";
+        description = "Directory for client configs (invitation TOML installs land here).";
+      };
+      stateDir = mkOption {
+        type = path;
+        default = "/var/lib/innernet-server";
+        description = "Server state directory (SQLite DB, keys, etc.).";
+      };
       listenPort = mkOption {
         type = port;
         default = 51820;
@@ -41,10 +50,10 @@ in {
         default = null;
         description = "Optional public endpoint (host:port) to advertise when initializing network.";
       };
-      extraArgs = mkOption {
-        type = listOf str;
-        default = [ ];
-        description = "Extra args to pass to innernet-server serve.";
+      cidr = mkOption {
+        type = str;
+        description = "The network cidr of the root network";
+        example = "10.100.0.0/16";
       };
     };
     # CLIENT
@@ -64,15 +73,15 @@ in {
   };
   
   config = mkIf (cfg.server.enable || cfg.client.enable) {
-    boot.kernel.sysctl = mkIf (cfg.servers != {}) {
+    boot.kernel.sysctl = mkIf (cfg.server != {}) {
       "net.ipv4.ip_forward" = 1;
       "net.ipv6.conf.all.forwarding" = 1;
     };
     
     environment.systemPackages = [ cfg.package pkgs.wireguard-tools ];
-    systemd.tmpfiles.rules = [
-      "d ${cfg.configDir} 0755 root root -"
-      "d ${cfg.stateDir}  0700 root root -"
+    systemd.tmpfiles.rules = mkIf cfg.server.enable [
+      "d ${cfg.server.configDir} 0755 root root -"
+      "d ${cfg.server.stateDir}  0700 root root -"
     ];
     
     networking.wireguard.enable = true;
@@ -86,13 +95,33 @@ in {
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
       requires = [ "network-online.target" ];
+      path = [ pkgs.iproute2 ];
+      environment = {
+        RUST_LOG = "info";
+      };
       serviceConfig = {
-        DynamicUser = false; # needs stable state dir
-        ExecStart = ''
-          ${lib.getExe' cfg.package "innernet-server"} serve ${netName} \
-            --state-dir ${cfg.stateDir} ${lib.concatStringsSep " " cfg.server.extraArgs}
+        ExecStartPre = pkgs.writeShellScript "innernet-systemd-server-pre-${netName}" ''
+          rm -f ${cfg.server.configDir}/${netName}.conf ${cfg.server.stateDir}/${netName}.db
+          ${lib.getExe' cfg.package "innernet-server"} new \
+            --network-name "${netName}" \
+            --network-cidr "${cfg.server.cidr}" \
+            --external-endpoint "${cfg.server.publicEndpoint}" \
+            --listen-port ${toString cfg.server.listenPort} >/dev/null
+          PKEY="$(cat ${cfg.privateKeyFile})"
+          export PKEY
+          sed -i "s|private-key =.*|private-key = \"$PKEY\"|g" ${cfg.server.configDir}/${netName}.conf
+          cat<<SQL | ${pkgs.sqlite}/bin/sqlite3 ${cfg.server.stateDir}/${netName}.db
+            UPDATE peers
+            SET public_key = '${cfg.publicKey}'
+            WHERE name = 'innernet-server';
+          SQL
         '';
-        WorkingDirectory = cfg.stateDir;
+        ExecStart = ''
+          ${lib.getExe' cfg.package "innernet-server"} \
+            --config-dir ${cfg.server.configDir} \
+            --data-dir ${cfg.server.stateDir} \
+            serve ${netName}
+        '';
         Restart = "on-failure";
         CapabilityBoundingSet = "CAP_NET_ADMIN CAP_NET_BIND_SERVICE";
         AmbientCapabilities = "CAP_NET_ADMIN CAP_NET_BIND_SERVICE";
