c6069698600dc624f8eead1682bc9eb77f112aa0
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2025-10-06 00:52:58 -0500

    chore(night): Initial

--- systems/prynthian/night/configuration.nix
+++ systems/prynthian/night/configuration.nix
@@ -4,14 +4,15 @@
     profiles = [
       "server"
     ];
+    
     host = {
       boot = {
-        kernel = pkgs.linuxPackages;
         loader = "systemd";
+        silentBoot = true;
       };
-      hardware.cpu = "intel";
-      network = {
-        tailscale.enable = true;
+      hardware = {
+        cpu = "intel";
+        gpu = "amd";
       };
       disk = {
         enable = true;
@@ -19,10 +20,50 @@
         device = "/dev/nvme0n1";
         impermanence = {
           enable = true;
-          rootSize = "2G";
+          rootSize = "1G";
         };
       };
     };
-    secrets.enable = false;
+    
+    services = {
+      tailscale.enable = true;
+      nebula = {
+        enable = false;
+        name = "cloud";
+        hostname = "cassian";
+        cidr = "10.24.13.7/24";
+        lighthouses = [ "10.24.13.254" ];
+        staticHostMap = {
+          "10.24.13.254" = [ "157.137.184.33:4242" ];
+        };
+        groups = [ "prynthian" ];
+        secrets = {
+          ca = "/run/secrets/nebula_ca/ca";
+          cert = "/run/secrets/nebula/crt";
+          key = "/run/secrets/nebula/key";
+        };
+        allowAll = true;
+      };
+    };
+    
+    secrets = {
+      enable = true;
+      items = {
+        "nebula_ca/ca" = {
+          key = "nebula_ca/cert";
+          owner = "nebula-cloud"; group = "nebula-cloud"; mode = "0400";
+        };
+        "nebula/crt" = {
+          sopsFile = "trove/hosts/night.yaml";
+          key = "nebula/crt";
+          owner = "nebula-cloud"; group = "nebula-cloud"; mode = "0400";
+        };
+        "nebula/key" = {
+          sopsFile = "trove/hosts/night.yaml";
+          key = "nebula/key";
+          owner = "nebula-cloud"; group = "nebula-cloud"; mode = "0400";
+       };
+      };
+    };
   };
 }
