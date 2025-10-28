0f4212a9fa983ff503b7562a6135019ac40e4854
Adam Arbour <845679+adamarbour@users.noreply.github.com>
2025-10-28 15:48:50 -0500

    chore(subs): Added vicinae

---
+++ modules/nixos/nix/substituters.nix
@@ -0,0 +1,11 @@
+{ pkgs, config, ... }:
+{
+  nix.settings = {
+    substituters = [
+      "https://vicinae.cachix.org"
+    ];
+    trusted-public-keys = [
+      "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+    ];
+  };
+}
